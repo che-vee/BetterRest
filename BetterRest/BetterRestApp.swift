957727cc9a3eca0ceac658c37b22eebfb22805e7
Che <vee.langat@gmail.com>
2021-02-08 12:42:19 +0400

    initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Chepkorir Lang'at on 03/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
